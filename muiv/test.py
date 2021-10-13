dff9c4a0b08bad571d53d365a285c8b4c48ca2fd
526_w1 <ec.qwertyui@gmail.com>
2021-10-13 12:20:31 +0300

    lec4 aip

--- muiv/test.py
+++ muiv/test.py
@@ -1,6 +1,7 @@
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+students = (
+    (("Студент", "Отчествович", "Фамильный"), 4),
+    (("Студентка", "Отчествовна", "Фамильная"), 5),
+)
+student_card = '''{0} {1} {2}: оценки: {3}'''
+print(student_card.format(students[0][0][0], students[0][0][1], students[0][0][2],
+                          " ".join(f"{subj}: {mark};" for subj, mark in students[0][1])))
