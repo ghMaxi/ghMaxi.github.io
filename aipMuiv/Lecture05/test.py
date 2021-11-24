fa1e0bb034a41a3faddd7fed0b3b7cffa724b691
526_w1 <ec.qwertyui@gmail.com>
2021-11-24 12:52:21 +0300

    test2 AiP

--- aipMuiv/Lecture05/test.py
+++ aipMuiv/Lecture05/test.py
@@ -1,3 +1,3 @@
 days_dict = {'today': (2021, "октябрь", 27), 'tomorrow': (2021, "октябрь", 28) }
-days_dict.pop('today')
-print(days_dict)
+days_dict2 = {'today2': (2022, "октябрь", 27), 'tomorrow2': (2022, "октябрь", 28) }
+print(days_dict + days_dict2)
